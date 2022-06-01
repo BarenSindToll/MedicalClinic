ec6c253192a5652f67a75b9b4b1aafb066f70db1
Melisa-chan <53099965+Melisa-chan@users.noreply.github.com>
2022-06-01 10:08:04 +0300

    Deleted speciality GUI for adding an appointment

--- src/main/java/com/example/medicalclinic/appointment/AppointmentList.java
+++ src/main/java/com/example/medicalclinic/appointment/AppointmentList.java
@@ -1,6 +1,5 @@
 package com.example.medicalclinic.appointment;
 
-import com.example.medicalclinic.specialty.Specialty;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/main/java/com/example/medicalclinic/controller/PatientController.java
+++ src/main/java/com/example/medicalclinic/controller/PatientController.java
@@ -1,4 +1,13 @@
 package com.example.medicalclinic.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class PatientController {
+    @FXML
+
+
+    public void addAppointment() {
+
+    }
 }
